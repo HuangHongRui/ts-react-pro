56a72b23f4b1570b18e1f30bf87f385d1fad73cf
Leo <huanghongrui@skysolutions.cn>
2017-12-27 13:27:33 +0800

    test

--- build/dist/head/index.js
+++
@@ -1,4 +0,0 @@
-import view from './nav';
-import antnav from './antHead';
-export { view, antnav };
-//# sourceMappingURL=index.js.map
--- build/dist/index.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import PageApp from './PageApp';
-ReactDOM.render(React.createElement(PageApp, null), document.getElementById('root'));
-//# sourceMappingURL=index.js.map
